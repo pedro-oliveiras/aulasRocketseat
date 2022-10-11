b791a1ea15c0bb3c0a096fe744018577613292fe
pedro oliveira <pedroh.poliveira2000@gmail.com>
2022-10-11 10:49:45 -0300

    organizacao e pricingtable

--- desafio6/countdown.js
+++ desafio6/countdown.js
@@ -14,19 +14,23 @@ var x = setInterval(function() {
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-
-  document.getElementById("day").innerHTML = days
-  document.getElementById("hour").innerHTML = hours
-  document.getElementById("minute").innerHTML = minutes
-  document.getElementById("second").innerHTML = seconds
-    
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);    
+  // Output the result in an element with id="demo"    
   // If the count down is over, write some text 
   if (distance < 0) {
     clearInterval(x);
+    document.getElementById("day").innerHTML = 00;
+    document.getElementById("hour").innerHTML = 00;
+    document.getElementById("minute").innerHTML = 00;
+    document.getElementById("second").innerHTML = 00;
   }
+  else {
+    document.getElementById("day").innerHTML = days
+    document.getElementById("hour").innerHTML = hours
+    document.getElementById("minute").innerHTML = minutes
+    document.getElementById("second").innerHTML = seconds
+  }  
+
 }, 1000);
 
 
