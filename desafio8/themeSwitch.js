db15ba2f364a55970a480b189e2f0d3151d51868
pedro <pedroh.poliveira2000@gmail.com>
2022-10-11 00:12:15 -0300

    pricingTables

--- desafio8/themeSwitch.js
+++ desafio8/themeSwitch.js
@@ -4,8 +4,27 @@ let button = document.getElementById('ball');
 
 let container = document.getElementById('content');
 let background = document.getElementById('background');
+let sun = document.getElementById('sun-img');
+let moon = document.getElementById('moon-img');
 
-if(container.style.justifyContent == ''){
+button.addEventListener('click', function(){
+    
+    if(moon.hasAttribute('hidden')){
+
+        sun.setAttribute('hidden', true);
+        moon.removeAttribute('hidden');
+        button.style.order = 1;
+        background.style.backgroundColor = '#292C35';
+    }
+    else {
+        moon.setAttribute('hidden', true);
+        sun.removeAttribute('hidden');
+        button.style.order = 0;
+        background.style.backgroundColor = '#F1F1F1';
+    }
+})
+
+/*if(container.style.justifyContent == ''){
     container.style.justifyContent = 'flex-start';
 }
 
@@ -19,4 +38,4 @@ button.addEventListener('click', function(){
         container.style.justifyContent = 'flex-start';
         background.style.backgroundColor = '#252525';
     }
-});
+});*/
